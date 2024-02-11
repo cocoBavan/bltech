602958c35f2c8c1bde410a559754c6e131febfd1
Bavan Palan <ambusedu@gmail.com>
2024-02-11 22:11:47 +1100

    Navigation bar animation was fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,11 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
-
-const poppins = Poppins({ weight: ['400', '700'],  subsets: ["latin"] });
+const poppins = Poppins({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "BL TechConsult",
@@ -22,13 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} container mx-auto`}>
-        <NavBar/>
-        <main className="relative overflow-hidden">
-        {children}
-        </main>
-        <Footer/>
-        </body>
+      <body className={`${poppins.className} mx-auto`}>
+        <NavBar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
