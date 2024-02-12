7f3aca0a58ec4861d90b9090e783831399ce95fb
Bavan Palan <ambusedu@gmail.com>
2024-02-12 23:17:31 +1100

    About page cleaned up.

--- dist/_next/static/chunks/app/page-6d74268889b007b8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{6226:function(e,n,t){Promise.resolve().then(t.t.bind(t,1749,23)),Promise.resolve().then(t.t.bind(t,5250,23)),Promise.resolve().then(t.t.bind(t,1533,23)),Promise.resolve().then(t.t.bind(t,4856,23))},1533:function(){},4856:function(){}},function(e){e.O(0,[647,971,69,744],function(){return e(e.s=6226)}),_N_E=e.O()}]);
