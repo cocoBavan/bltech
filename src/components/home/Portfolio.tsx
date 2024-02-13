786b44b06ae242c65a025daa871ec20fa3b43d0e
Bavan Palan <ambusedu@gmail.com>
2024-02-13 22:57:55 +1100

    Added Titler

--- src/components/home/Portfolio.tsx
+++ src/components/home/Portfolio.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import "./css/portfolio.css";
+import Tilter from "../common/Tilter";
 
 interface PortfolioItem {
   imageUrl: string;
@@ -14,29 +15,31 @@ interface PortfolioItem {
 const PortfolioCard = ({ item }: { item: PortfolioItem }) => {
   return (
     <li className="mb-[13px] pl-[13px] w-1/3">
-      <div className="list_inner tilt-effect w-full h-auto clear-both float-left relative overflow-hidden">
-        <div className="image relative">
-          <img
-            className="relative opacity-0 min-w-full"
-            src="/assets/img/thumbs/1-1.jpg"
-            alt=""
-          />
-          <div
-            style={{
-              backgroundImage: `url("${item.imageUrl}")`,
-            }}
-            className="main absolute inset-0 bg-no-repeat bg-cover bg-center rounded-[12px]"
-          ></div>
+      <Tilter>
+        <div className="list_inner tilt-effect w-full h-auto clear-both relative overflow-hidden">
+          <div className="image relative">
+            <img
+              className="relative opacity-0 min-w-full"
+              src="/assets/img/thumbs/1-1.jpg"
+              alt=""
+            />
+            <div
+              style={{
+                backgroundImage: `url("${item.imageUrl}")`,
+              }}
+              className="main absolute inset-0 bg-no-repeat bg-cover bg-center rounded-[12px]"
+            ></div>
+          </div>
+          <div className="details absolute z-[2] bottom-[20px] left-[20px] right-[20px] bg-[#1e1345] p-[20px] rounded-[12px] translate-y-[125%] transition-all duration-300">
+            <h3 className="text-[20px] mb-[2px]">{item.title}</h3>
+            <span className="text-[16px]">{item.subtitle}</span>
+          </div>
+          <Link
+            className="foliox_tm_full_link absolute inset-0 z-[5] popup-youtube"
+            href={item.portfolioLink}
+          ></Link>
         </div>
-        <div className="details absolute z-[2] bottom-[20px] left-[20px] right-[20px] bg-[#1e1345] p-[20px] rounded-[12px] translate-y-[125%] transition-all duration-300">
-          <h3 className="text-[20px] mb-[2px]">{item.title}</h3>
-          <span className="text-[16px]">{item.subtitle}</span>
-        </div>
-        <Link
-          className="foliox_tm_full_link absolute inset-0 z-[5] popup-youtube"
-          href={item.portfolioLink}
-        ></Link>
-      </div>
+      </Tilter>
     </li>
   );
 };
@@ -82,11 +85,8 @@ const items: PortfolioItem[] = [
 
 const Portfolio = () => {
   return (
-    <div
-      className="foliox_tm_section w-full h-auto clear-both float-left"
-      id="portfolio"
-    >
-      <div className="foliox_tm_portfolio w-full h-auto clear-both float-left mb-[150px]">
+    <div className="foliox_tm_section w-full h-auto clear-both" id="portfolio">
+      <div className="foliox_tm_portfolio w-full h-auto clear-both  mb-[150px]">
         <div className="container">
           <div className="foliox_tm_main_title w-full max-w-[650px] h-auto clear-both my-0 mx-auto text-center">
             <span className="text-[21px] font-poppins text-extra-color font-medium inline-block mb-[5px]">
@@ -94,7 +94,7 @@ const Portfolio = () => {
             </span>
             <h3 className="text-[48px]">What I Do for My Clients</h3>
           </div>
-          <div className="portfolio_filter w-full h-auto clear-both float-left text-center mt-[60px] mb-[55px]">
+          <div className="portfolio_filter w-full h-auto clear-both text-center mt-[60px] mb-[55px]">
             <ul>
               <li className="mr-[35px] inline-block">
                 <Link
@@ -153,7 +153,7 @@ const Portfolio = () => {
             </ul>
           </div>
           <div
-            className="portfolio_list wow fadeInUp w-full h-auto clear-both float-left"
+            className="portfolio_list wow fadeInUp w-full h-auto clear-both"
             data-wow-duration="1s"
             data-wow-delay="0.2s"
           >
